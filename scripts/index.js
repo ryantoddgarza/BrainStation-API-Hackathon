fcfb78553dc57160a64013994e560d6bb0206b71
ryantoddgarza <ryantoddgarza@gmail.com>
2020-06-10 09:49:28 -0700

    axios

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+// scratch data i used to get things working originally
+// const url = 'https://api.openweathermap.org/data/2.5/weather?q=Burbank&units=imperial&appid=84a3db98d0d8c0cf2b5702f8cd8244a8';
+
+// https.get(url, (response) => {
+//   response.on('data', function(data) {
+//     const weatherData = JSON.parse(data);
+//     const temp = weatherData.main.temp;
+//     const description = weatherData.weather[0].description;
+//     res.send(`The temperature in Burbank is ${temp} degrees farenheight`);
+//   });
+
+// });
