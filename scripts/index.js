a4fd323dbfcdaa63cea3c04be002e6a79100d6bf
ryantoddgarza <ryantoddgarza@gmail.com>
2020-06-10 10:45:59 -0700

    open weather api integration

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,29 @@
-// scratch data i used to get things working originally
-// const url = 'https://api.openweathermap.org/data/2.5/weather?q=Burbank&units=imperial&appid=84a3db98d0d8c0cf2b5702f8cd8244a8';
+// weathermap api
+getWeather = (usrLat, usrLon) => {
+  const key = '84a3db98d0d8c0cf2b5702f8cd8244a8'
+  const units = 'metric';
+  const lat = usrLat;
+  const lon = usrLon;
 
-// https.get(url, (response) => {
-//   response.on('data', function(data) {
-//     const weatherData = JSON.parse(data);
-//     const temp = weatherData.main.temp;
-//     const description = weatherData.weather[0].description;
-//     res.send(`The temperature in Burbank is ${temp} degrees farenheight`);
-//   });
+  const url = `https://api.openweathermap.org/data/2.5/find?lat=${lat}&lon=${lon}&units=${units}&appid=${key}`;
 
-// });
+  axios.get(url)
+    .then((response) => {
+      console.log(response);
+    })
+    .catch(() => console.error(`Could not resolve ${url}`));
+}
+
+window.onload = getWeather(lon, lat);
+
+
+
+
+
+
+
+// create a new element
+const newEl = (tag) => {
+  return document.createElement(tag);
+}
+
